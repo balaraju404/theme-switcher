98aa06defb739f6c61e666b94691589d7728672d
Balaraju Gandham <balarajugandham1@gmail.com>
2025-02-07 21:55:12 +0530

    Update layout.route.ts

--- src/app/layout/layout.route.ts
+++ src/app/layout/layout.route.ts
@@ -4,7 +4,7 @@ import { LayoutComponent } from "./layout.component";
 export const layoutRoutes: Routes = [
  {
   path: "", component: LayoutComponent, children: [
-   { path: "", redirectTo: "create-theme", pathMatch: "full" },
+  // { path: "", redirectTo: "create-theme", pathMatch: "full" },
    { path: "create-theme", loadComponent: () => import("./components/create-theme/create-theme.component").then((m) => m.CreateThemeComponent) }
   ]
  }
